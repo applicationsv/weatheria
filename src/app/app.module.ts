6ffed5a2ea73add1a6eef7faff4f94a7ea8b5957
anonymoussc <50c5ac69@opayq.com>
2018-06-17 18:30:30 +0700

    Modified src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { WeatherService } from './weather/weather.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     CurrentWeatherComponent
   ],
-  imports: [
-    BrowserModule
+  imports     : [
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers   : [WeatherService],
+  bootstrap   : [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
