9320460bfcb0a781ea3542927cdc8ab1fe5154e0
anonymoussc <50c5ac69@opayq.com>
2018-06-17 16:10:03 +0700

    New weather.service.ts

---
+++ src/app/weather/weather.service.ts
@@ -0,0 +1,10 @@
+interface ICurrentWeatherData {
+  weather: [{
+    description: string,
+    icon: string
+  }],
+  main: { temp: number },
+  sys: { country: string },
+  dt: number,
+  name: string
+}
