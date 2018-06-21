7a68540916324ce5be9fabd3dca9863e8dc2e8c0
anonymoussc <50c5ac69@opayq.com>
2018-06-21 18:01:11 +0700

    Update weather

--- src/app/weather/weather.service.fake.ts
+++ src/app/weather/weather.service.fake.ts
@@ -1,9 +1,9 @@
-import { Observable, of } from 'rxjs';
-import { IWeatherService } from './weather.service';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { ICurrentWeather } from '../interfaces';
+import { IWeatherService } from './weather.service';
 
 export class WeatherServiceFake implements IWeatherService {
-  private fakeWeather: ICurrentWeather = <ICurrentWeather>{
+  private fakeWeather: ICurrentWeather = {
     city       : 'Bursa',
     country    : 'TR',
     date       : 1485789600,
@@ -12,7 +12,16 @@ export class WeatherServiceFake implements IWeatherService {
     description: 'light intensity drizzle',
   };
 
-  public getCurrentWeather(city: string, country: string): Observable<ICurrentWeather> {
+  currentWeather = new BehaviorSubject<ICurrentWeather>(this.fakeWeather);
+
+  public getCurrentWeather(city: string | number,
+    country?: string): Observable<ICurrentWeather> {
     return of(this.fakeWeather);
+  }
+
+  public updateCurrentWeather(search: string | number, country?: string) {
+    this.getCurrentWeather(search, country).subscribe(weather =>
+      this.currentWeather.next(weather)
+    );
   }
 }
