dba329a4cbebab98b55ccfe4a7b3ec2119ab1d00
anonymoussc <50c5ac69@opayq.com>
2018-06-21 10:50:00 +0700

    Modified src/app/weather/weather.service.ts

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -22,8 +22,12 @@ interface ICurrentWeatherData {
   name: string;
 }
 
+export interface IWeatherService {
+  getCurrentWeather(city: string, country: string): Observable<ICurrentWeather>;
+}
+
 @Injectable()
-export class WeatherService {
+export class WeatherService implements IWeatherService {
   constructor(private httpClient: HttpClient) {
   }
 
