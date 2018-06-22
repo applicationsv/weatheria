9cd101b1874025f67a7824ec2a3f35fbbba5f789
anonymoussc <50c5ac69@opayq.com>
2018-06-22 08:59:15 +0700

    Modified src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
+import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
+import { CitySearchTpldrivenComponent } from './city-search-tpldriven/city-search-tpldriven.component';
+import { CitySearchComponent } from './city-search/city-search.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
 import { WeatherService } from './weather/weather.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CurrentWeatherComponent
+    CurrentWeatherComponent,
+    CitySearchComponent,
+    CitySearchTpldrivenComponent,
   ],
   imports     : [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers   : [WeatherService],
-  bootstrap   : [AppComponent]
+  bootstrap   : [AppComponent],
 })
 export class AppModule {
 }
