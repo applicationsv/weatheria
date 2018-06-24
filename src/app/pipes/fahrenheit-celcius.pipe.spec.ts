dbf9f108d41b2e3fc6a53870f883ffb9dd5a61fb
anonymoussc <50c5ac69@opayq.com>
2018-06-24 10:16:56 +0700

    Deleted fahrenheit-celcius.pipe.spec.ts

--- src/app/pipes/fahrenheit-celcius.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FahrenheitCelciusPipe } from './fahrenheit-celcius.pipe';
-
-describe('FahrenheitCelciusPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FahrenheitCelciusPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
